109822392e74829645aea682ca68269b8fccc13a
samutamm <samutamm@gmail.com>
2016-06-14 19:19:54 +0200

    Fix dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM frolvlad/alpine-oraclejdk8:slim
+RUN mkdir -p src/main/java/server/models/
 VOLUME /tmp
 ADD build/libs/gs-spring-boot-docker-0.1.0.jar app.jar
 RUN sh -c 'touch /app.jar'
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,17 +1,12 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-import java.io.IOException;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws IOException {
-        //ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        String rootfile = System.getProperty("user.dir");
-        File file = new File(rootfile + "/src/main/java/server/models/newFile.txt");
-        System.out.println("CAN CREATE A FILE!");
-        System.out.println(file.createNewFile());
+    public static void main(String[] args) throws Exception {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 }
